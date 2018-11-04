20f97b841608221ee07ef0ba153544f1df583c29
jtCodes <jt234wq@gmail.com>
2018-11-04 14:56:03 -0500

    first

---
+++ apple-music-card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apple-music-card
+//
+//  Created by J Tan on 11/4/18.
+//  Copyright © 2018 J Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ apple-music-cardTests/apple_music_cardTests.swift
@@ -0,0 +1,34 @@
+//
+//  apple_music_cardTests.swift
+//  apple-music-cardTests
+//
+//  Created by J Tan on 11/4/18.
+//  Copyright © 2018 J Tan. All rights reserved.
+//
+
+import XCTest
+@testable import apple_music_card
+
+class apple_music_cardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
